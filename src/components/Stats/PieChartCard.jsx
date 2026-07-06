2560dbd4b97f9c9304bb8e1d736379e855ecd72f
Ivan Cervantes <153766241+Ivancvnts@users.noreply.github.com>
2026-07-06 16:20:45 -0600

    fix: overflow issues and design improvement

--- src/components/Stats/PieChartCard.jsx
+++ src/components/Stats/PieChartCard.jsx
@@ -1,27 +1,34 @@
 import { useContext } from "react";
 import { UsersContext } from "../../contexts/UsersContext.js";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
 
 function PieChartCard({ data, title, colors, totalUsers }) {
   return (
-    <div className="pie-char-card">
-      <p className="pie-char-card__title">{title}</p>
-      <div className="pie-char-card__content">
-        <div className="pie-char-card__container">
-          <PieChart width={270} height={320}>
-            <Pie data={data} innerRadius={80} outerRadius={120} dataKey="value">
-              {data.map((entry, index) => (
-                <Cell key={index} fill={colors[index]} />
-              ))}
-            </Pie>
-            <Tooltip />
-          </PieChart>
-          <div className="pie-char-card__total">
-            <p className="pie-char-card__stat">{totalUsers}</p>
-            <p className="pie-char-card__description">Total</p>
+    <div className="pie-chart-card">
+      <p className="pie-chart-card__title">{title}</p>
+      <div className="pie-chart-card__content">
+        <div className="pie-chart-card__container">
+          <ResponsiveContainer width="100%" height={270}>
+            <PieChart>
+              <Pie
+                data={data}
+                innerRadius={80}
+                outerRadius={120}
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell key={index} fill={colors[index]} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+          <div className="pie-chart-card__total">
+            <p className="pie-chart-card__stat">{totalUsers}</p>
+            <p className="pie-chart-card__description">Total</p>
           </div>
         </div>
-        <ul className="pie-char-card__role-list">
+        <ul className="pie-chart-card__role-list">
           {data.map((role, index) => (
             <li key={role.name}>
               <span style={{ background: colors[index] }}></span>
